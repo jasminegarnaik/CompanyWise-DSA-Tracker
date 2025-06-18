76f92c9ff6182d63c1b132d76edcf35f2f01a64f
jasminegarnaik <141756116+jasminegarnaik@users.noreply.github.com>
2025-06-18 12:09:47 +0530

    Update and rename Find the Highest Altitude to Find the Highest Altitude.java

--- Cisco/Find the Highest Altitude.java
+++ Cisco/Find the Highest Altitude.java
@@ -1,4 +1,4 @@
-## Approach
+/* Approach
 1.Initialize the variable currentAltitude to 0; this is the current altitude of the biker.
 2.Initialize the variable highestPoint to currentAltitude, as the highest altitude we have seen is 0.
 3.Iterate over the gain in altitude in the list gain and add the current gain altitudeGain to the variable currentAltitude.
@@ -7,8 +7,8 @@
 
 ## Leetcode Link-
 https://leetcode.com/problems/find-the-highest-altitude/description/
-
-##JAVA Code
+*/
+//JAVA Code
 class Solution {
     public int largestAltitude(int[] gain) {
         int currentAlt=0;
